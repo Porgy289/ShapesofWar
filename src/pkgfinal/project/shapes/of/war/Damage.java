88709114a6c0dfeab94b579ca86215fd9a0eca69
Peyton Dan-Sonier <747420@04-315-J0CP4KR.SD41.ADS>
2021-04-20 11:14:46 -0700

    Commit

--- src/pkgfinal/project/shapes/of/war/Damage.java
+++ src/pkgfinal/project/shapes/of/war/Damage.java
@@ -2,6 +2,85 @@
 package pkgfinal.project.shapes.of.war;
 
 
-public class Damage {
+
+public class Damage{
+
+    private String name;
+    private double HP;
+    private double CurrentHP;
+    private double ATK;
+    private double DEF;
+
+  
+    public Damage(String createName, int createMaxHP, int createSTR, int createDEF) {
+
+        name = createName;
+        HP = createMaxHP;
+        CurrentHP = createMaxHP;
+        ATK = createSTR;
+        DEF = createDEF;
+    }
+
+  
+    public String getName() {
+        return name;
+    }
+
+    public double getHP() {
+        return CurrentHP;
+    }
+
+    public void setHP(int hp) {
+        CurrentHP = hp;
+    }
+
+    public double HP() {
+        return HP;
+    }
+
+    public void setMaxHP(int newMaxHp) {
+        HP = newMaxHp;
+    }
+
+    public double getATK() {
+        return ATK;
+    }
+
+    public void setSTR(int newSTR)
+    {
+        ATK = newSTR;
+    }
+
+    public double getDEF() {
+        return DEF;
+    }
+
+    public void setDEF(int newDEF){
+        DEF = newDEF;
+    }
     
+    public void takeDamage(int atk){
+        if (DEF>0) 
+            CurrentHP=CurrentHP-(atk*(1-DEF));
+        else
+            CurrentHP = CurrentHP-atk;
+    }
+    
+    public void startHP(){
+        CurrentHP=HP;
+    }
+
+    //Return ALL values
+    public void getInfo() {
+        System.out.println("");
+        System.out.println("\t-=|STATS|=-");
+        System.out.println("-------------------");
+        System.out.println("Name: \t" + name);
+        System.out.println("HP: \t" + CurrentHP + "/" + HP);
+        System.out.println("STR: \t" + ATK);
+        System.out.println("DEF: \t" + DEF);
+    }
+
 }
+  
+
--- src/pkgfinal/project/shapes/of/war/FinalProjectShapesOfWar.java
+++ src/pkgfinal/project/shapes/of/war/FinalProjectShapesOfWar.java
@@ -9,5 +9,6 @@ public class FinalProjectShapesOfWar {
     public static void main(String[] args) {
         
     }
+   
     
 }
