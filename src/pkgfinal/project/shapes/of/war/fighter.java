1335e3ebe25b4ad43779a1b50687a67ef3f9969c
747124 <82898067+747124@users.noreply.github.com>
2021-04-21 09:57:56 -0700

    Update fighter.java

--- src/pkgfinal/project/shapes/of/war/fighter.java
+++ src/pkgfinal/project/shapes/of/war/fighter.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pkgfinal.project.shapes.of.war;
 
-/**
- *
- * @author 747420
- */
-public class fighter {
+
+public class fighter extends Minion {
+    private int pos;
+    private int hp;
+    private int maxhp;
+    private int x;
+    private int y;
+    private int width;
+    private int height;
     
+    public fighter (){
+        
 }
