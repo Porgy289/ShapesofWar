28027085eae6f6d247a96f9d66b051f6c553d61d
747124 <82898067+747124@users.noreply.github.com>
2021-04-21 09:35:44 -0700

    Update Minion.java

--- src/pkgfinal/project/shapes/of/war/Minion.java
+++ src/pkgfinal/project/shapes/of/war/Minion.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pkgfinal.project.shapes.of.war;
 
-/**
- *
- * @author 747420
- */
+
 public class Minion {
+    protected int hp;
+    protected int maxhp;
+    protected int damage;
+    
+}
+
+public void act(){
+    move();
+}
+
+public void move(){
+    if (forward){
+        if (position==maxpos)
+            Attack();
+        else{
+            position++;
+        }
+    }
     
 }
