e7fe85aa1625e2ad3c5b87a3a1af84369bf2de4a
747124 <82898067+747124@users.noreply.github.com>
2021-04-22 20:04:12 -0700

    Update ranged.java

--- src/pkgfinal/project/shapes/of/war/ranged.java
+++ src/pkgfinal/project/shapes/of/war/ranged.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pkgfinal.project.shapes.of.war;
 
-/**
- *
- * @author 747420
- */
-public class ranged {
-    
+
+import java.awt.*;
+import java.util.ArrayList;
+
+
+public class Ranged extends Minion
+{
+    public Ranged( int x, int y )
+    {
+        super( x, y, 100, 50 );
+    }
+
+    public void shoot( ArrayList<Bullet> bullets, Minion target )
+    {
+        double dx = target.x - x;
+        double dy = target.y - y;
+        double dz = Math.sqrt( dx*dx + dy*dy );
+        bullets.add( new Bullet( x, y, 8 * dx / dz, 8 * dy / dz ) );
+    }
+
+    public void draw( Graphics2D g )
+    {
+        g.setColor( Color.BLUE );
+        g.fillRect( getX() - getWidth(), getY() - getWidth(), 2 * getWidth(), 2 * getWidth() );
+        g.setColor( Color.RED );
+        g.fillOval( getX() - getWidth() / 2, getY() - getWidth() / 2, getWidth(), getWidth());
+    }
 }
+
